a79d9d7f7aa64b23434b86053f81b5793de5e8ae
Jake Vo <jakevo@jakes-mac-mini.lan>
2021-01-22 21:25:05 -0800

    update structure

--- contactController.js
+++ contactController.js
@@ -9,12 +9,13 @@ exports.index = function (req, res) {
                 status: "error",
                 message: err,
             });
-        }
-        res.json({
-            status: "success",
-            message: "Contacts retrieved successfully",
-            data: contacts
-        });
+        } else {
+            res.json({
+                status: "success",
+                message: "Contacts retrieved successfully",
+                data: contacts
+            });
+        }       
     });
 };
 // Handle create contact actions
@@ -28,7 +29,7 @@ exports.new = function (req, res) {
     contact.save(function (err) {
         // if (err)
         //     res.json(err);
-res.json({
+        res.json({
             message: 'New contact created!',
             data: contact
         });
@@ -47,22 +48,24 @@ exports.view = function (req, res) {
 };
 // Handle update contact info
 exports.update = function (req, res) {
-Contact.findById(req.params.contact_id, function (err, contact) {
+    Contact.findById(req.params.contact_id, function (err, contact) {
         if (err)
             res.send(err);
-contact.name = req.body.name ? req.body.name : contact.name;
-        contact.gender = req.body.gender;
-        contact.email = req.body.email;
-        contact.phone = req.body.phone;
-// save the contact and check for errors
-        contact.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'Contact Info updated',
-                data: contact
+        else {
+            contact.name = req.body.name ? req.body.name : contact.name;
+            contact.gender = req.body.gender;
+            contact.email = req.body.email;
+            contact.phone = req.body.phone;
+            // save the contact and check for errors
+            contact.save(function (err) {
+                if (err)
+                    res.json(err);
+                res.json({
+                    message: 'Contact Info updated',
+                    data: contact
+                });
             });
-        });
+        }
     });
 };
 // Handle delete contact
@@ -72,7 +75,7 @@ exports.delete = function (req, res) {
     }, function (err, contact) {
         if (err)
             res.send(err);
-res.json({
+    res.json({
             status: "success",
             message: 'Contact deleted'
         });
